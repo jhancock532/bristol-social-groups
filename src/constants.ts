5529144e6590393b0b75526424b490511e93d0ab
james.hancock@torchbox.com <james.hancock@torchbox.com>
2024-06-28 15:32:04 +0100

    Add new contstants file, extract constant variables

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import { join } from 'path';
+
+export const WEEKDAYS = [
+    'None',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+];
+
+export const GROUP_DATA_FILE_PATH = join(process.cwd(), 'data/groups');
