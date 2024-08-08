9b905953a2cbfaff9208bc9bc56302cf5f39b47e
james.hancock@torchbox.com <james.hancock@torchbox.com>
2024-08-08 17:22:54 +0100

    Add hot air balloons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,22 +6,16 @@ export const Footer = () => {
     return (
         <footer className={styles.container}>
             <div></div>
-            <div>
-                <p className={styles.description}>
-                    This website is{' '}
-                    <Link url="/about" className={styles.link}>
-                        a volunteer run initiative
-                    </Link>{' '}
-                    in early development.
+            <div className={styles.description}>
+                <p>
+                    <strong>Thank you for visiting!</strong>
+                </p>
+                <p>
+                    This website is a non-profit, volunteer led initiative.
                     <br />
                     If you&apos;d like to leave feedback,{' '}
                     <Link url="/feedback" className={styles.link}>
                         please use this form
-                    </Link>
-                    .<br />
-                    You can also suggest a group to be added to the site{' '}
-                    <Link url="/add-group" className={styles.link}>
-                        on this page
                     </Link>
                     .
                 </p>
