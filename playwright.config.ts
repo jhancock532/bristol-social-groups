681a387f5eea5f6fd834c76bd51ee1e089128e08
james.hancock@torchbox.com <james.hancock@torchbox.com>
2024-08-14 17:01:02 +0100

    Code review updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,12 +5,8 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
     testDir: './src',
-    /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
@@ -18,18 +14,11 @@ export default defineConfig({
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-        /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: process.env.PLAYWRIGHT_BASE_URL,
-
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
     },
-
-    /* Configure projects for major browsers */
     projects: [
         {
             name: 'desktop-chrome',
@@ -52,11 +41,4 @@ export default defineConfig({
             use: { ...devices['iPhone 12'] },
         },
     ],
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //     command: 'npm run storybook',
-    //     url: 'http://127.0.0.1:6006',
-    //     reuseExistingServer: !process.env.CI,
-    // },
 });
