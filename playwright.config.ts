3843991a99b1ac61c2c0aa44d954ab485a77fa40
james.hancock@torchbox.com <james.hancock@torchbox.com>
2024-08-14 16:58:32 +0100

    Add playwright

---
+++ playwright.config.ts
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    testDir: './src',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'html',
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL: process.env.PLAYWRIGHT_BASE_URL,
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry',
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'desktop-chrome',
+            use: { ...devices['Desktop Chrome'] },
+        },
+        {
+            name: 'desktop-firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+        {
+            name: 'desktop-safari',
+            use: { ...devices['Desktop Safari'] },
+        },
+        {
+            name: 'mobile-chrome',
+            use: { ...devices['Pixel 5'] },
+        },
+        {
+            name: 'mobile-safari',
+            use: { ...devices['iPhone 12'] },
+        },
+    ],
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //     command: 'npm run storybook',
+    //     url: 'http://127.0.0.1:6006',
+    //     reuseExistingServer: !process.env.CI,
+    // },
+});
