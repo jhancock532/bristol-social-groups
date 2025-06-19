855ade2de1cbe309a887a7d7df3728445b97b4cf
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-06-19 12:04:12 +0500

    Set up Jest config with next/jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-        '\\.(scss|sass|css)$': 'identity-obj-proxy',
-        '^@/(.*)$': '<rootDir>/src/$1', // optional path alias
-    },
-    testMatch: ['**/__tests__/**/*.(test|spec).(js|jsx|ts|tsx)'],
-    transform: {
-        '^.+\\.[jt]sx?$': 'babel-jest',
-    },
-    transformIgnorePatterns: ['/node_modules/'],
-};
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+module.exports = createJestConfig({
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.(test|spec).(js|jsx|ts|tsx)'],
+});
