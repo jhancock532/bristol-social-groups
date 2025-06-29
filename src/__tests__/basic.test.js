3383f8418acc4e5d4d8bf53afa6280826de379ce
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-06-29 18:20:25 +0500

    delete unnecessary basic.test file

--- src/__tests__/basic.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-test('renders hello message', () => {
-    render(<h1>Hello, Bristol!</h1>);
-    expect(screen.getByText(/hello, bristol!/i)).toBeInTheDocument();
-});
