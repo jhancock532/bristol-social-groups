9f4cae18a76f1f4ffa3d8a0a9a7edd8e963fe003
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-06-29 16:45:26 +0500

    convert js to ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,17 +3,17 @@ import nextJest from 'next/jest';
 import type { Config } from '@jest/types';
 
 const createJestConfig = nextJest({
-  dir: './',
+    dir: './',
 });
 
 const customJestConfig: Config.InitialOptions = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(scss|sass|css)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  testMatch: ['**/__tests__/**/*.(test|spec).(js|jsx|ts|tsx)'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '\\.(scss|sass|css)$': 'identity-obj-proxy',
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: ['**/__tests__/**/*.(test|spec).(js|jsx|ts|tsx)'],
 };
 
 export default createJestConfig(customJestConfig);
